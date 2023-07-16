c320fe836bcff197fa1f7fa401b1fe3a4cfc161d
Thophylarh <iwualathophylarh@gmail.com>
2023-07-16 22:04:55 +0100

    axios setup

---
+++ src/lib/axios.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+// Create an instance of Axios with default configuration
+const axiosInstance = axios.create({
+  baseURl: process.env.REACT_APP_BASE_URL, // Your API base URL
+
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Add request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error)
+);
+
+// Add response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => response,
+
+  (error) => Promise.reject(error)
+);
+
+export default axiosInstance;
